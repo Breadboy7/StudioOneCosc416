f54e1b92847171b23aadf9b9ae4eb7255627ea8d
Brett <brett.mckenzie3@gmail.com>
2025-01-14 08:48:29 -0800

    Update BallRoll.cs
    Added BallRoll

--- Assets/BallRoll.cs
+++ Assets/BallRoll.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class BallRoll : MonoBehaviour
 {
+    public Rigidbody sphereRigidbody;
+    public float ballSpeed = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,21 @@ public class BallRoll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 movementVector = Vector2.zero;
+        //Check for user input
+        if (Input.GetKey(KeyCode.W))
+            movementVector += Vector2.up;
+        else if (Input.GetKey(KeyCode.A))
+            movementVector += Vector2.left;
+        else if (Input.GetKey(KeyCode.S))
+            movementVector += Vector2.down;
+        else if (Input.GetKey(KeyCode.D))
+            movementVector += Vector2.right;
+
+        Vector3 inputXZPlane = new Vector3(movementVector.x, 0, movementVector.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed * Time.deltaTime);
+        Debug.Log("Resultant Vector: " + movementVector);
+        Debug.Log("Resultant 3D Vector: " + movementVector);
+
     }
 }
